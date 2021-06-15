06a83a7f86e24d8fd5e72c942867410a62845d7a
MariaJans <mariapjansian@gmail.com>
2021-06-16 00:27:14 +0300

    Step 4 - Let's complicate the API

--- index.js
+++ index.js
@@ -7,20 +7,42 @@ app.get("/", (req, res) => {
 }
 );
 //creating simple API
-app.get("/test",(req, res) => {
+app.get("/test", (req, res) => {
     res.send({
-        status:200, 
-        message:"ok"
+        status: 200,
+        message: "ok"
     })
 });
 
-app.get("/time",(req, res) => {
+app.get("/time", (req, res) => {
     let date = new Date();
     let hour = date.getHours();
     let minute = date.getMinutes();
     res.send({
-        status:200,
+        status: 200,
         message: hour + ":" + minute,
     })
+})
+app.get("/hello/:id(\\d+)", (req, res) => {
+    res.send({
+        status: 200,
+        message: "Hello " + req.params.id,
+    })
+})
+app.get("/search", (req, res) => {
+    const search = req.query.s;
+    if (search) {
+        res.send({
+            status: 200,
+            message: "ok",
+            data: search,
+        })
+    } else {
+        res.send({
+            status: 500,
+            error: true,
+            message: "you have to provide a search",
+        })
+    }
 })
 app.listen(3000);
