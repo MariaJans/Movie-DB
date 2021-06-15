72b030bd0e450963c9b5dfd905a325b34273845e
MariaJans <mariapjansian@gmail.com>
2021-06-15 23:29:53 +0300

    Step 3 - Create an express simple API

--- index.js
+++ index.js
@@ -6,4 +6,21 @@ app.get("/", (req, res) => {
     res.send("ok");
 }
 );
+//creating simple API
+app.get("/test",(req, res) => {
+    res.send({
+        status:200, 
+        message:"ok"
+    })
+});
+
+app.get("/time",(req, res) => {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    res.send({
+        status:200,
+        message: hour + ":" + minute,
+    })
+})
 app.listen(3000);
